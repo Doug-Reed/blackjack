d9f38cb56eb921f10544773c37dda90848e3d25f
Doug Reed <doug.reed@wisc.edu>
2018-11-15 07:51:31 -0600

    jabberwocky

--- src/main/java/blackjack/action/PlayerAction.java
+++ src/main/java/blackjack/action/PlayerAction.java
@@ -1,10 +1,11 @@
 package blackjack.action;
 
 import blackjack.model.Table;
+import blackjack.model.Player;
 public abstract class PlayerAction {
 
     public String actionIs(){
         return "abstract";
     }
-    public void go(Table table) {}
+    public void go(Table table, Player player) {}
 }
